28032ca42dcfcece3e6724bc68c88210c7ec37a1
unknown <1900011032@pku.edu.cn>
2023-03-11 11:13:45 +0800

    爬山算法加注释

--- homework/hw2/python/queens_hill_climb.py
+++ homework/hw2/python/queens_hill_climb.py
@@ -6,24 +6,23 @@ from problem.queens_swap import QueensSwapState
 from algorithm.hill_climb import HillClimb
 from utils.selection import MaxSelection, FirstBetterSelection, RouletteSelection
 
-
 if __name__ == "__main__":
-    n = 20
+    n = 40
     log_n = log(n)
-    queens_move_state_value_estimator = lambda state : exp(-log_n * state.conflicts)
-    queens_swap_state_value_estimator = lambda state : exp(-log_n * state.state.conflicts)
-    
+    queens_move_state_value_estimator = lambda state: exp(-log_n * state.conflicts)  # ==1/(n^conflicts)
+    queens_swap_state_value_estimator = lambda state: exp(-log_n * state.state.conflicts)
+
     t0 = time()
-    f_selection = FirstBetterSelection()
-    r_selection = RouletteSelection()
-    m_selection = MaxSelection()
-    
-    # state = QueensMoveState(n)
-    # hcs = HillClimb(state)
-    # hcs.search(queens_move_state_value_estimator, 1.0, n<<2, f_selection, 5)
-    
-    state = QueensSwapState(n)
+    f_selection = FirstBetterSelection()  # 第一最佳选择 能找到的话 想必也是最快的
+    r_selection = RouletteSelection()  # 轮盘赌选择
+    m_selection = MaxSelection()  # 最大估值选择
+
+    state = QueensMoveState(n)
     hcs = HillClimb(state)
-    hcs.search(queens_swap_state_value_estimator, 1.0, n<<2, f_selection, 5)
-    
+    hcs.search(queens_move_state_value_estimator, 1.0, n << 2, f_selection, 5)
+
+    # state = QueensSwapState(n)
+    # hcs = HillClimb(state)
+    # hcs.search(queens_swap_state_value_estimator, 1.0, n << 2, f_selection, 5)  # target_value是1.0 意味着conflict得为0
+
     print(time() - t0)
--- homework/hw2/python/utils/selection.py
+++ homework/hw2/python/utils/selection.py
@@ -3,94 +3,97 @@ import sys
 
 from .random_variables import RandomVariables
 
+
 class SelectionBase(metaclass=ABCMeta):
-    
+
     @abstractmethod
-    def initialize(self, items:int, initial_value:float) -> None:
+    def initialize(self, items: int, initial_value: float) -> None:
         raise NotImplementedError
-    
+
     @abstractmethod
-    def submit(self, value:float) -> None:
+    def submit(self, value: float) -> None:
         raise NotImplementedError
-    
+
     @abstractmethod
     def done(self) -> bool:
         raise NotImplementedError
-    
+
     @abstractmethod
     def selected_index(self) -> int:
         raise NotImplementedError
 
+
 class RouletteSelection(SelectionBase):
-    
+
     def __init__(self):
         pass
-    
-    def initialize(self, items:int, initial_value:float) -> None:
+
+    def initialize(self, items: int, initial_value: float) -> None:
         self._value_sum = 0
         self._index = 0
         self._total_items = items
         self._submitted_items = 0
-        
-    def submit(self, value:float) -> None:
+
+    def submit(self, value: float) -> None:  # 需要遍历全部，每次选择会有权重 value/value_sum 与value成正比
+        # 这种方式第k个被选择的概率是value_k/sum_value_k*sum_value_k/sum_value_k+1*...*sum_value_n-1/sum_value_n正好等于value_k在全部n个状态value和中所占比。
         assert value >= 0
         self._value_sum += value
         if RandomVariables.uniform_real() < value / (self._value_sum + sys.float_info.min):
             self._index = self._submitted_items
         self._submitted_items += 1
-    
+
     def done(self) -> bool:
         return self._submitted_items >= self._total_items
-    
+
     def selected_index(self) -> int:
         return self._index
 
 
 class FirstBetterSelection(SelectionBase):
-    
+
     def __init__(self):
         pass
-    
-    def initialize(self, items:int, initial_value:float) -> None:
-        
+
+    def initialize(self, items: int, initial_value: float) -> None:
         self._target_value = initial_value
         self._index = 0
         self._total_items = items
         self._submitted_items = 0
         self._item_found = False
-        
-    def submit(self, value:float) -> None:
+
+    def submit(self, value: float) -> None:  # 只要出现大于原有value的状态，就item_found，记录index
         if value > self._target_value:
             self._item_found = True
             self._index = self._submitted_items
-        
+
         self._submitted_items += 1
-    
+
     def done(self) -> bool:
         return self._item_found or self._submitted_items >= self._total_items
-    
+
     def selected_index(self) -> int:
         return self._index
-    
+
+
 class MaxSelection(SelectionBase):
-    
+
     def __init__(self):
         pass
-    
-    def initialize(self, items:int, initial_value:float) -> None:
+
+    def initialize(self, items: int, initial_value: float) -> None:
         self._index = 0
         self._total_items = items
         self._submitted_items = 0
-    
-    def submit(self, value:float) -> None:
+
+    def submit(self, value: float) -> None:  # 需要全部遍历，记录最大value状态
         if self._submitted_items == 0 or value > self._max_value:
             self._max_value = value
             self._index = self._submitted_items
-        
+
         self._submitted_items += 1
-    
+
     def done(self) -> bool:
         return self._submitted_items >= self._total_items
-    
+
     def selected_index(self) -> int:
         return self._index
